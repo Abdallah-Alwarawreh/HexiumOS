20a5bfe42c2e6524e0abb41668a1d4987ff3762b
Lexi <charsarg@spifftech.uk>
2026-02-11 15:14:20 +0000

    Update hex_fetch.rs

--- src/hex_fetch.rs
+++ src/hex_fetch.rs
@@ -8,9 +8,24 @@ pub struct HexFetch {
 impl HexFetch {
 
 pub fn fetch(writer: &mut Writer){
-writer.write_str("OS: Hexium OS x86_64\n");
-writer.write_str("Other stuff lol idk :3\n");
+    writer.set_color(Color::LightCyan, Color::Black);
+    writer.write_str("  _    _           _                  ____   _____ ");
 
+    writer.write_str(" OS: HexiumOS\n");
+  
+    writer.write_str(" | |  | |         (_)                / __ \\ / ____|");
+
+    writer.write_str(" Version: N/A \n");
+  
+    writer.write_str(" | |__| | _____  ___ _   _ _ __ ___ | |  | | (___  \n");
+    writer.write_str(" |  __  |/ _ \\ \\/ / | | | | '_ ` _ \\| |  | |\\___ \\ \n");
+    writer.write_str(" | |  | |  __/>  <| | |_| | | | | | | |__| |____) |\n");
+    writer.write_str(" |_|  |_|\\___/_/\\_\\_|\\__,_|_| |_| |_|\\____/|_____/ \n");
+    writer.write_str("                                               \n");
+    writer.write_str("                                               \n");
+
+    
+  
 }
 
 
